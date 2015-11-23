3ad9a829a334959f7a71f42a9dd18f6e1ffc7936
qqq3 <aiqcms@gmail.com>
2015-11-23 20:58:27 +0300

    Add variable: query and app id

--- app/src/main/java/org/asdtm/goodweather/WeatherRequest.java
+++ app/src/main/java/org/asdtm/goodweather/WeatherRequest.java
@@ -3,4 +3,9 @@ package org.asdtm.goodweather;
 
 public class WeatherRequest
 {
+    //http://api.openweathermap.org/data/2.5/weather?q=london&units=metric&APPID=7b1eaeea7795f54d52027369812383d0
+    private final static String QUERY = "api.openweathermap.org/data/2.5/weather?q=";
+    private final static String APPID = "APPID=7b1eaeea7795f54d52027369812383d0";
+
+
 }
