4501cc4992929f75073c7a43d7d361086ad8c16a
qqq3 <aiqcms@gmail.com>
2015-11-27 10:00:28 +0300

    Изменен метод получения данных

--- app/src/main/java/org/asdtm/goodweather/WeatherRequest.java
+++ app/src/main/java/org/asdtm/goodweather/WeatherRequest.java
@@ -2,6 +2,7 @@ package org.asdtm.goodweather;
 
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -14,33 +15,39 @@ public class WeatherRequest
     private final static String QUERY = "api.openweathermap.org/data/2.5/weather?q=";
     private final static String APPID = "7b1eaeea7795f54d52027369812383d0";
 
-    public String getWeather(String location) throws IOException
+    byte[] getWeatherByte(String location) throws IOException
     {
+        // Создаем объект URL
         URL url = new URL(location);
+        // Вызов метода openConnection() создает объект подключения к заданному url-адресу
         HttpURLConnection connection= (HttpURLConnection) url.openConnection();
 
         try {
-
-            StringBuffer buffer = new StringBuffer();
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
             InputStream inputStream = connection.getInputStream();
-            BufferedReader bufferedReader =
-                    new BufferedReader(new InputStreamReader(inputStream));
 
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                buffer.append(line);
+            if (connection.getResponseCode() != connection.HTTP_OK) {
+                return null;
             }
-            inputStream.close();
 
-            return buffer.toString();
+            int bytesRead = 0;
+            byte[] buffer = new byte[1024];
+
+            while ((bytesRead = inputStream.read()) > 0)
+            {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+            outputStream.close();
+
+            return outputStream.toByteArray();
         } finally {
             connection.disconnect();
         }
-
     }
+
 
     public String getUrl(String url) throws IOException
     {
-        return new String(getWeather(url));
+        return new String(getWeatherByte(url));
     }
 }
