9d5fd4397a7fe82b079997192041f3860f67b41b
qqq3 <aiqcms@gmail.com>
2015-11-30 15:00:48 +0300

    Toolbar work with support.v4.Fragment

--- app/src/main/java/org/asdtm/goodweather/WeatherPageFragment.java
+++ app/src/main/java/org/asdtm/goodweather/WeatherPageFragment.java
@@ -1,10 +1,11 @@
 package org.asdtm.goodweather;
 
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -49,7 +50,10 @@ public class WeatherPageFragment extends Fragment
         View v = inflater.inflate(R.layout.fragment_main, parent, false);
 
         mToolbar = (Toolbar) v.findViewById(R.id.toolbar);
-        ((ActionBarActivity)getActivity()).setSupportActionBar(mToolbar);
+
+        AppCompatActivity appCompatActivity = (AppCompatActivity) getActivity();
+        appCompatActivity.setSupportActionBar(mToolbar);
+
 
         mTemperatureView = (TextView) v.findViewById(R.id.temperature);
         mDescription = (TextView) v.findViewById(R.id.weather_description);
@@ -58,7 +62,10 @@ public class WeatherPageFragment extends Fragment
         mWindSpeed = (TextView) v.findViewById(R.id.wind_speed);
         mClouds = (TextView) v.findViewById(R.id.clouds);
 
+
         mNewRequest = (SwipeRefreshLayout) v.findViewById(R.id.new_request);
+        int top_to_padding = 150;
+        mNewRequest.setProgressViewOffset(false, 0, top_to_padding);
         mNewRequest.setColorSchemeResources(R.color.swipe_red,
                 R.color.swipe_green,
                 R.color.swipe_blue);
