7f3b498f7d7759f140f9deb5199b9f3eb4dcddc0
qqq3 <aiqcms@gmail.com>
2015-12-01 13:25:39 +0300

    Add variable and overriding onBackPressed

--- app/src/main/java/org/asdtm/goodweather/MainActivity.java
+++ app/src/main/java/org/asdtm/goodweather/MainActivity.java
@@ -1,14 +1,33 @@
 package org.asdtm.goodweather;
 
 import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends SingleFragmentActivity
 {
+    protected static String stateFragment = "MainActivity";
+
     @Override
     protected Fragment createNewFragment()
     {
         return new WeatherPageFragment();
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        switch (stateFragment) {
+            case "WeatherPageFragment":
+                DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+                if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    mDrawerLayout.closeDrawer(GravityCompat.START);
+                } else {
+                    super.onBackPressed();
+                }
+                break;
+        }
     }
 }
