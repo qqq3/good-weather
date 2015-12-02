9c5925753d84938749bdcbd6b6023a81246f5dde
qqq3 <aiqcms@gmail.com>
2015-12-02 11:10:51 +0300

    Add onCreate, onCreateView method

---
+++ app/src/main/java/org/asdtm/goodweather/SettingsFragment.java
@@ -0,0 +1,25 @@
+package org.asdtm.goodweather;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+public class SettingsFragment extends Fragment
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState)
+    {
+        View v = inflater.inflate(R.layout.fragment_settings, parent, false);
+
+        return v;
+    }
+}
