0eb5913b22c9393d80863d365b46943508bc59db
qqq3 <aiqcms@gmail.com>
2015-12-02 11:42:35 +0300

    Fix error

--- app/src/main/java/org/asdtm/goodweather/SettingsFragment.java
+++ app/src/main/java/org/asdtm/goodweather/SettingsFragment.java
@@ -5,7 +5,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewParent;
 
 public class SettingsFragment extends Fragment
 {
