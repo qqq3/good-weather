8bf620c13804f76fcbe3996b8ef990b16c0d34d4
qqq3 <aiqcms@gmail.com>
2015-12-02 11:49:32 +0300

    Initialize toolbar

--- app/src/main/java/org/asdtm/goodweather/SettingsFragment.java
+++ app/src/main/java/org/asdtm/goodweather/SettingsFragment.java
@@ -2,12 +2,15 @@ package org.asdtm.goodweather;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class SettingsFragment extends Fragment
 {
+    private Toolbar mToolbar;
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
@@ -19,6 +22,8 @@ public class SettingsFragment extends Fragment
     {
         View v = inflater.inflate(R.layout.fragment_settings, parent, false);
 
+        mToolbar = (Toolbar) v.findViewById(R.id.toolbar);
+        
         return v;
     }
 }
