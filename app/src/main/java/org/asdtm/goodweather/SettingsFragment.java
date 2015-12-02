2bcd4b8a318ef7db8d62f59beeeb15c9165ad3b1
qqq3 <aiqcms@gmail.com>
2015-12-02 11:51:26 +0300

    Add toolbar into layout

--- app/src/main/java/org/asdtm/goodweather/SettingsFragment.java
+++ app/src/main/java/org/asdtm/goodweather/SettingsFragment.java
@@ -2,6 +2,7 @@ package org.asdtm.goodweather;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,7 +24,10 @@ public class SettingsFragment extends Fragment
         View v = inflater.inflate(R.layout.fragment_settings, parent, false);
 
         mToolbar = (Toolbar) v.findViewById(R.id.toolbar);
-        
+
+        AppCompatActivity appCompatActivity = (AppCompatActivity) getActivity();
+        appCompatActivity.setSupportActionBar(mToolbar);
+
         return v;
     }
 }
