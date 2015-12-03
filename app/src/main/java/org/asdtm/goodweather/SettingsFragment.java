8399d3038cae18f2c2536c5c615d02787daf5490
qqq3 <aiqcms@gmail.com>
2015-12-03 12:51:39 +0300

    Add adapter

--- app/src/main/java/org/asdtm/goodweather/SettingsFragment.java
+++ app/src/main/java/org/asdtm/goodweather/SettingsFragment.java
@@ -1,5 +1,6 @@
 package org.asdtm.goodweather;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.NavUtils;
@@ -9,6 +10,18 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.asdtm.goodweather.model.CitySearch;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SettingsFragment extends Fragment
 {
@@ -32,7 +45,110 @@ public class SettingsFragment extends Fragment
         AppCompatActivity appCompatActivity = (AppCompatActivity) getActivity();
         appCompatActivity.setSupportActionBar(mToolbar);
         appCompatActivity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        AutoCompleteTextView mSearchCity = (AutoCompleteTextView) v.findViewById(R.id.autoComplete_search_city);
+        CityAdapter cityAdapter = new CityAdapter(getActivity(), null);
+        mSearchCity.setAdapter(cityAdapter);
+
+        final TextView mCurrentCity = (TextView) v.findViewById(R.id.currentCity);
+        mSearchCity.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
+                CitySearch result = (CitySearch) parent.getItemAtPosition(position);
+                mCurrentCity.setText("Current city: " + result.getCityName());
+            }
+        });
         return v;
+    }
+
+    private class CityAdapter extends ArrayAdapter<CitySearch> implements Filterable
+    {
+        private Context mContext;
+        private List<CitySearch> listCity = new ArrayList<CitySearch>();
+
+        public CityAdapter(Context context, List<CitySearch> list)
+        {
+            super(context, R.layout.result_search_city_layout, list);
+            mContext = context;
+            listCity = list;
+        }
+
+        @Override
+        public int getCount()
+        {
+            if (listCity != null)
+                return listCity.size();
+
+            return 0;
+        }
+
+        @Override
+        public CitySearch getItem(int index)
+        {
+            if (listCity != null)
+                return listCity.get(index);
+
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position)
+        {
+            if (listCity != null) {
+                return position;
+            }
+
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent)
+        {
+            if (convertView == null) {
+                LayoutInflater inflater = LayoutInflater.from(mContext);
+                convertView = inflater.inflate(R.layout.result_search_city_layout, parent, false);
+            }
+
+            TextView resultSearch = (TextView) convertView.findViewById(R.id.result_search);
+            resultSearch.setText(listCity.get(position).getCityName()
+                    + ", "
+                    + listCity.get(position).getCountry());
+
+
+            return convertView;
+        }
+
+        @Override
+        public Filter getFilter()
+        {
+            Filter cityFilter = new Filter()
+            {
+                @Override
+                protected FilterResults performFiltering(CharSequence constraint)
+                {
+                    FilterResults filterResults = new FilterResults();
+                    if (constraint == null || constraint.length() < 3)
+                        return filterResults;
+
+                    List<CitySearch> citySearchList = YahooParser.getCity(constraint.toString());
+                    filterResults.values = citySearchList;
+                    filterResults.count = citySearchList.size();
+
+                    return filterResults;
+                }
+
+                @Override
+                protected void publishResults(CharSequence constraint, FilterResults results)
+                {
+                    listCity = (List) results.values;
+                    notifyDataSetChanged();
+                }
+            };
+
+            return cityFilter;
+        }
     }
 
     @Override
