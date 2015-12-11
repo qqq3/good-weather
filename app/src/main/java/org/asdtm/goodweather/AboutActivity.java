b1d694318a32c22974b9df0ee245513974eacd86
qqq3 <aiqcms@gmail.com>
2015-12-11 10:48:38 +0300

    Add onCreate method

--- app/src/main/java/org/asdtm/goodweather/AboutActivity.java
+++ app/src/main/java/org/asdtm/goodweather/AboutActivity.java
@@ -1,7 +1,14 @@
 package org.asdtm.goodweather;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class AboutActivity extends AppCompatActivity
 {
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
 }
