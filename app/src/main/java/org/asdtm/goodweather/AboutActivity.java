c9f68c3a10f77b9f9f8a4daeb40159419244bea6
qqq3 <aiqcms@gmail.com>
2015-12-11 19:11:30 +0300

    Add onOptionsItemSelected method

--- app/src/main/java/org/asdtm/goodweather/AboutActivity.java
+++ app/src/main/java/org/asdtm/goodweather/AboutActivity.java
@@ -2,7 +2,9 @@ package org.asdtm.goodweather;
 
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity
@@ -25,5 +27,19 @@ public class AboutActivity extends AppCompatActivity
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                if (NavUtils.getParentActivityName(this) != null) {
+                    NavUtils.navigateUpFromSameTask(this);
+                    return true;
+                }
+        }
+        
+        return super.onOptionsItemSelected(item);
     }
 }
