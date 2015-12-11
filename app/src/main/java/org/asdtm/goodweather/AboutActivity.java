937c40455a10dd72e0e8dc894d4c85a941a5bdd1
qqq3 <aiqcms@gmail.com>
2015-12-11 14:01:04 +0300

    Set versionName to program_version_textView

--- app/src/main/java/org/asdtm/goodweather/AboutActivity.java
+++ app/src/main/java/org/asdtm/goodweather/AboutActivity.java
@@ -1,14 +1,29 @@
 package org.asdtm.goodweather;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity
 {
+    private TextView mAppVersion;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        mAppVersion = (TextView) findViewById(R.id.program_version_textView);
+
+        try {
+            String versionName = getPackageManager()
+                    .getPackageInfo(getPackageName(), 0)
+                    .versionName;
+            mAppVersion.setText(versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
