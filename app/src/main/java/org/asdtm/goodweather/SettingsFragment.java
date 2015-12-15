2243f8391be8b08157877ec4e6ebc28cb8048c82
qqq3 <aiqcms@gmail.com>
2015-12-15 21:23:28 +0300

    Del cardView import

--- app/src/main/java/org/asdtm/goodweather/SettingsFragment.java
+++ app/src/main/java/org/asdtm/goodweather/SettingsFragment.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
