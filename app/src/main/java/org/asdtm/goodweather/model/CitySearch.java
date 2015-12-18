8977615cb4e5ae7d85e250fde9a7342be11927cf
qqq3 <aiqcms@gmail.com>
2015-12-18 15:16:50 +0300

    Add longitude and latitude on CitySearch model

--- app/src/main/java/org/asdtm/goodweather/model/CitySearch.java
+++ app/src/main/java/org/asdtm/goodweather/model/CitySearch.java
@@ -4,12 +4,16 @@ public class CitySearch
 {
     private String mCityName;
     private String mCountry;
+    private float mLatitude;
+    private float mLongitude;
 
     public CitySearch(){}
-    public CitySearch(String cityName, String country)
+    public CitySearch(String cityName, String country, float latitude, float longitude)
     {
         mCityName = cityName;
         mCountry = country;
+        mLatitude = latitude;
+        mLongitude = longitude;
     }
 
     public String getCityName()
@@ -30,6 +34,26 @@ public class CitySearch
     public void setCountry(String country)
     {
         mCountry = country;
+    }
+
+    public float getLatitude()
+    {
+        return mLatitude;
+    }
+
+    public void setLatitude(float latitude)
+    {
+        mLatitude = latitude;
+    }
+
+    public float getLongitude()
+    {
+        return mLongitude;
+    }
+
+    public void setLongitude(float longitude)
+    {
+        mLongitude = longitude;
     }
 
     @Override
