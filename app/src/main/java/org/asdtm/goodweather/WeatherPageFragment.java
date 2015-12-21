929e1325672c62a16abce91750c36aa51ee02859
qqq3 <aiqcms@gmail.com>
2015-12-21 18:36:29 +0300

    Check api, if api=23 than check permission

--- app/src/main/java/org/asdtm/goodweather/WeatherPageFragment.java
+++ app/src/main/java/org/asdtm/goodweather/WeatherPageFragment.java
@@ -1,17 +1,21 @@
 package org.asdtm.goodweather;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -414,16 +418,9 @@ public class WeatherPageFragment extends Fragment
 
         switch (item.getItemId()) {
             case R.id.menu_find_location:
-                if (isGPSEnabled) {
-                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-                                                           0,
-                                                           0,
-                                                           mLocationListener);
-                } else if (isNetworkEnabled){
-                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
-                                                           0,
-                                                           0,
-                                                           mLocationListener);
+                if (isGPSEnabled && isNetworkEnabled) {
+                    gpsRequestLocation();
+                    networkRequestLocation();
                 } else {
                     showSettingsAlert();
                 }
@@ -437,20 +434,38 @@ public class WeatherPageFragment extends Fragment
         @Override
         public void onLocationChanged(Location location)
         {
-            mSharedPreferences = getActivity().getSharedPreferences(APP_SETTINGS,
-                                                                    Context.MODE_PRIVATE);
-            SharedPreferences.Editor editor = mSharedPreferences.edit();
-
             String latitude = String.format("%1$.2f", location.getLatitude());
             String longitude = String.format("%1$.2f", location.getLongitude());
 
+            Log.d(TAG, "Current location: " + latitude + ";" + longitude);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (ActivityCompat.checkSelfPermission(getActivity(),
+                                                       Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        getActivity(),
+                        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+            }
+            locationManager.removeUpdates(mLocationListener);
+
+            /* isInternetConnection = false;
+            connectionDetector = new ConnectionDetector(getContext());
+            isInternetConnection = connectionDetector.connectToInternet();*/
+
+            mSharedPreferences = getActivity().getSharedPreferences(APP_SETTINGS,
+                                                                    Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = mSharedPreferences.edit();
             editor.putString(APP_SETTINGS_LATITUDE, latitude);
             editor.putString(APP_SETTINGS_LONGITUDE, longitude);
             editor.apply();
-
-            Log.d(TAG, "Current location: " + latitude + ";" + longitude);
-
-            locationManager.removeUpdates(mLocationListener);
         }
 
         @Override
@@ -480,25 +495,72 @@ public class WeatherPageFragment extends Fragment
 
         settingsAlert.setPositiveButton(R.string.alertDialog_gps_positiveButton,
                                         new DialogInterface.OnClickListener()
-        {
-            @Override
-            public void onClick(DialogInterface dialog, int which)
-                {
-                    Intent goToSettings = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                    startActivity(goToSettings);
-                }
-        });
+                                        {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which)
+                                            {
+                                                Intent goToSettings = new Intent(
+                                                        Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                                                startActivity(goToSettings);
+                                            }
+                                        });
 
         settingsAlert.setNegativeButton(R.string.alertDialog_gps_negativeButton,
                                         new DialogInterface.OnClickListener()
-        {
-            @Override
-            public void onClick(DialogInterface dialog, int which)
-            {
-                dialog.cancel();
-            }
-        });
+                                        {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which)
+                                            {
+                                                dialog.cancel();
+                                            }
+                                        });
 
         settingsAlert.show();
+    }
+
+    public void gpsRequestLocation()
+    {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(getActivity(),
+                                                   Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    getActivity(),
+                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                                               0,
+                                               0,
+                                               mLocationListener);
+    }
+
+    public void networkRequestLocation()
+    {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(getActivity(),
+                                                   Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    getActivity(),
+                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+        }
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                                               0,
+                                               0,
+                                               mLocationListener);
     }
 }
