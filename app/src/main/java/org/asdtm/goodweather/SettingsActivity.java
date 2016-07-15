0dd7c908ca3536a13a42506f90b80f40c1f1519d
qqq3 <aiqcms@gmail.com>
2016-07-15 12:07:10 +0300

    Change pref screen

--- app/src/main/java/org/asdtm/goodweather/SettingsActivity.java
+++ app/src/main/java/org/asdtm/goodweather/SettingsActivity.java
@@ -1,12 +1,96 @@
 package org.asdtm.goodweather;
 
-import android.support.v4.app.Fragment;
+import android.annotation.TargetApi;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
+import android.view.MenuItem;
 
-public class SettingsActivity extends SingleFragmentActivity
-{
+public class SettingsActivity extends AppCompatPreferenceActivity {
+
+    public static final String KEY_PREF_TEMPERATURE = "temperature_pref_key";
+
     @Override
-    protected Fragment createNewFragment()
-    {
-        return new SettingsFragment();
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setupActionBar();
+
+        getFragmentManager().beginTransaction()
+                            .replace(android.R.id.content, new GeneralPreferenceFragment())
+                            .commit();
+    }
+
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    protected boolean isValidFragment(String fragmentName) {
+        return PreferenceFragment.class.getName().equals(fragmentName)
+                || GeneralPreferenceFragment.class.getName().equals(fragmentName);
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public static class GeneralPreferenceFragment extends PreferenceFragment implements
+            SharedPreferences.OnSharedPreferenceChangeListener {
+
+        SharedPreferences mPreferences;
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.pref_general);
+            setHasOptionsMenu(true);
+
+            mPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        }
+
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            switch (key) {
+                case KEY_PREF_TEMPERATURE:
+                    Preference temperaturePref = findPreference(key);
+                    temperaturePref.setSummary(sharedPreferences.getString(key, "metric")
+                                                                .equals("metric") ? "°C" : "°F");
+            }
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            getPreferenceScreen().getSharedPreferences()
+                                 .registerOnSharedPreferenceChangeListener(this);
+            Preference temperaturePref = findPreference(KEY_PREF_TEMPERATURE);
+            temperaturePref.setSummary(
+                    mPreferences.getString(KEY_PREF_TEMPERATURE, "metric")
+                                .equals("metric") ? "°C" : "°F");
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            getPreferenceScreen().getSharedPreferences()
+                                 .unregisterOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            switch (item.getItemId()) {
+                case android.R.id.home:
+                    if (NavUtils.getParentActivityName(getActivity()) != null) {
+                        NavUtils.navigateUpFromSameTask(getActivity());
+                        return true;
+                    }
+            }
+
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
