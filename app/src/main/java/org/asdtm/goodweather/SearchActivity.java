013985ed285c0f1df42233837914e7a2ae601cba
qqq3 <aiqcms@gmail.com>
2016-07-18 22:32:11 +0300

    Fix error on load last found city into RecyclerView

--- app/src/main/java/org/asdtm/goodweather/SearchActivity.java
+++ app/src/main/java/org/asdtm/goodweather/SearchActivity.java
@@ -223,7 +223,7 @@ public class SearchActivity extends AppCompatActivity implements SearchView.OnQu
     private void loadLastFoundCity() {
         if (mCites.isEmpty()) {
             String lastCity = mCityPref.getString(APP_SETTINGS_CITY, "London");
-            String lastCountry = mCityPref.getString(APP_SETTINGS_COUNTRY, "UK");
+            String lastCountry = mCityPref.getString(APP_SETTINGS_COUNTRY_CODE, "UK");
             String lastLat = mCityPref.getString(APP_SETTINGS_LATITUDE, "51.51");
             String lastLon = mCityPref.getString(APP_SETTINGS_LONGITUDE, "-0.13");
             CitySearch city = new CitySearch(lastCity, lastCountry, lastLat, lastLon);
