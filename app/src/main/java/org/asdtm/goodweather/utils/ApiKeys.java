3b30bc79883d2b1d57c5a5b5f40446778f422177
qqq3 <aiqcms@gmail.com>
2016-08-16 08:13:34 +0300

    Add api keys

---
+++ app/src/main/java/org/asdtm/goodweather/utils/ApiKeys.java
@@ -0,0 +1,14 @@
+package org.asdtm.goodweather.utils;
+
+public class ApiKeys {
+
+    /**
+     * API key for http://openweathermap.org/api
+     */
+    public static final String OPEN_WEATHER_MAP_API_KEY = "1487dd8a93bfd85d278d9ac8dcfee94c";
+
+    /**
+     * API key for Yahoo GeoPlanet
+     */
+    public static final String WHERE_YAHOO_API_KEY = "bff5f4884257ae36fbf385bfa93facb09bfbb53b";
+}
