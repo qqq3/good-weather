5cdcd1bd93af41c38c77b6946b03a2c7ec5efa61
qqq3 <aiqcms@gmail.com>
2016-08-18 16:07:07 +0300

    Move weather preference keys to PrefKeys class

--- app/src/main/java/org/asdtm/goodweather/utils/PrefKeys.java
+++ app/src/main/java/org/asdtm/goodweather/utils/PrefKeys.java
@@ -6,7 +6,7 @@ public class PrefKeys {
      * SharedPreference names
      */
     public static final String APP_SETTINGS_NAME = "config";
-
+    public static final String PREF_WEATHER_NAME = "weather_pref";
 
     public static final String APP_SETTINGS_LOCALE = "locale";
     public static final String APP_SETTINGS_LATITUDE = "latitude";
@@ -15,4 +15,12 @@ public class PrefKeys {
     public static final String KEY_PREF_TEMPERATURE = "temperature_pref_key";
     public static final String KEY_PREF_INTERVAL_NOTIFICATION = "notification_interval_pref_key";
     public static final String KEY_PREF_VIBRATE = "notification_vibrate_pref_key";
+
+    public static final String WEATHER_DATA_TEMPERATURE = "temperature";
+    public static final String WEATHER_DATA_DESCRIPTION = "description";
+    public static final String WEATHER_DATA_PRESSURE = "pressure";
+    public static final String WEATHER_DATA_HUMIDITY = "humidity";
+    public static final String WEATHER_DATA_WIND_SPEED = "wind_speed";
+    public static final String WEATHER_DATA_CLOUDS = "clouds";
+    public static final String WEATHER_DATA_ICON = "icon";
 }
