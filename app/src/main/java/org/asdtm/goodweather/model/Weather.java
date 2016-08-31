5a67a2795dd66c5f5f2e527637a0683dee87e92e
qqq3 <aiqcms@gmail.com>
2016-08-31 16:06:38 +0300

    Added sunrise and sunset into weather model

--- app/src/main/java/org/asdtm/goodweather/model/Weather.java
+++ app/src/main/java/org/asdtm/goodweather/model/Weather.java
@@ -1,139 +1,126 @@
 package org.asdtm.goodweather.model;
 
-public class Weather
-{
+public class Weather {
+
     public CitySearch location;
     public Temperature temperature = new Temperature();
     public Wind wind = new Wind();
     public CurrentWeather currentWeather = new CurrentWeather();
     public CurrentCondition currentCondition = new CurrentCondition();
     public Cloud cloud = new Cloud();
+    public Sys sys = new Sys();
 
-    public class Temperature
-    {
+    public class Temperature {
+
         private float mTemp;
-        private float mMinTemp;
-        private float mMaxTemp;
 
-        public float getTemp()
-        {
+        public float getTemp() {
             return mTemp;
         }
-        public void setTemp(float temp)
-        {
+
+        public void setTemp(float temp) {
             mTemp = temp;
-        }
-
-        public float getMinTemp()
-        {
-            return mMinTemp;
-        }
-        public void setMinTemp(float minTemp)
-        {
-            mMinTemp = minTemp;
-        }
-
-        public float getMaxTemp()
-        {
-            return mMaxTemp;
-        }
-
-        public void setMaxTemp(float maxTemp)
-        {
-            mMaxTemp = maxTemp;
         }
     }
 
-    public class Wind
-    {
+    public class Wind {
+
         private float mSpeed;
         private float mDirection;
 
-        public float getSpeed()
-        {
+        public float getSpeed() {
             return mSpeed;
         }
 
-        public void setSpeed(float speed)
-        {
+        public void setSpeed(float speed) {
             mSpeed = speed;
         }
 
-        public float getDirection()
-        {
+        public float getDirection() {
             return mDirection;
         }
 
-        public void setDirection(float direction)
-        {
+        public void setDirection(float direction) {
             mDirection = direction;
         }
     }
 
-    public class CurrentWeather
-    {
+    public class CurrentWeather {
+
         private String mDescription;
         private String mIdIcon;
 
-        public String getDescription()
-        {
+        public String getDescription() {
             return mDescription;
         }
 
-        public void setDescription(String description)
-        {
+        public void setDescription(String description) {
             mDescription = description;
         }
 
-        public String getIdIcon()
-        {
+        public String getIdIcon() {
             return mIdIcon;
         }
 
-        public void setIdIcon(String idIcon)
-        {
+        public void setIdIcon(String idIcon) {
             mIdIcon = idIcon;
         }
     }
 
-    public class CurrentCondition
-    {
+    public class CurrentCondition {
+
         private float mPressure;
         private int mHumidity;
 
-        public float getPressure()
-        {
+        public float getPressure() {
             return mPressure;
         }
 
-        public void setPressure(float pressure)
-        {
+        public void setPressure(float pressure) {
             mPressure = pressure;
         }
 
-        public int getHumidity()
-        {
+        public int getHumidity() {
             return mHumidity;
         }
 
-        public void setHumidity(int humidity)
-        {
+        public void setHumidity(int humidity) {
             mHumidity = humidity;
         }
     }
 
-    public class Cloud
-    {
+    public class Cloud {
+
         private int mClouds;
 
-        public int getClouds()
-        {
+        public int getClouds() {
             return mClouds;
         }
 
-        public void setClouds(int clouds)
-        {
+        public void setClouds(int clouds) {
             mClouds = clouds;
+        }
+    }
+
+    public class Sys {
+
+        private long mSunrise;
+        private long mSunset;
+
+        public long getSunrise() {
+            return mSunrise;
+        }
+
+        public void setSunrise(long sunrise) {
+            mSunrise = sunrise;
+        }
+
+        public long getSunset() {
+            return mSunset;
+        }
+
+        public void setSunset(long sunset) {
+            mSunset = sunset;
         }
     }
 }
